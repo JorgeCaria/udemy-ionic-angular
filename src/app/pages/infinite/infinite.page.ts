3980f5884524ac88af1399f5780779dfe4cb2633
JorgeDev <93055253+JorgeCaria@users.noreply.github.com>
2022-09-27 17:49:07 +0200

    ion-list

---
+++ src/app/pages/infinite/infinite.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { Timeouts } from 'selenium-webdriver';
+
+@Component({
+  selector: 'app-infinite',
+  templateUrl: './infinite.page.html',
+  styleUrls: ['./infinite.page.scss'],
+})
+export class InfinitePage implements OnInit {
+
+  data : any[] = Array(20);
+
+  @ViewChild(IonInfiniteScroll) infiniteScroll : IonInfiniteScroll;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  loadData( event ){
+
+    //console.log(event);
+
+    setTimeout(() => {
+
+      if(this.data.length > 50){
+          this.infiniteScroll.complete();
+          this.infiniteScroll.disabled = true;
+          return;
+      }
+
+      const nuevoArr = Array(20);
+      this.data.push(...nuevoArr)
+
+      //event.target.complete();
+      this.infiniteScroll.complete();
+
+    }, 1500);
+  }
+
+}
---
+++ src/app/pages/list/list.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+ 
+import { IonicModule } from '@ionic/angular';
+
+import { ListPageRoutingModule } from './list-routing.module';
+
+import { ListPage } from './list.page';
+import { ComponentsModule } from '../../components/components.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ListPageRoutingModule,
+    ComponentsModule,
+    HttpClientModule
+  ],
+  declarations: [ListPage]
+})
+export class ListPageModule {}
